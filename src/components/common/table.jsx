653aadc566b62c1779b0b77f855481fec8e652a4
Federico Aviles <federico.aviles@devsar.com>
2020-08-27 14:49:36 -0300

    refactoring

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 
-const Table = (props) => {
-  const { columns, data, onSort, sortColumn } = props;
+const Table = ({ columns, data, onSort, sortColumn }) => {
   return (
     <table className="table">
       <TableHeader columns={columns} onSort={onSort} sortColumn={sortColumn} />
