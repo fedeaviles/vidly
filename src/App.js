d1457181fdd0c2b3da56ba2fe422f9c551dd1eb6
Federico Aviles <federico.aviles@devsar.com>
2020-08-28 12:51:42 -0300

    add forms and validation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rentals from "./components/rentals";
 import { Switch, Route, Redirect } from "react-router-dom";
 import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Redirect exact from="/" to="/movies" />
