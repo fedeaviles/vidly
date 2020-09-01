0253c866cfce0534a74be626d7cdfdacc2984a4a
Federico Aviles <federico.aviles@devsar.com>
2020-09-01 09:59:17 -0300

    add jwt header to axios

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -14,10 +14,15 @@ axios.interceptors.response.use(null, (error) => {
   return Promise.reject(error);
 });
 
+export function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
 export default {
   get: axios.get,
   post: axios.post,
   delete: axios.delete,
   patch: axios.patch,
   put: axios.put,
+  setJwt,
 };
